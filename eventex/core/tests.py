9231e384f53e053d60e21b741568496a6df7ed7a
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-09-24 22:45:25 -0300

    test(index): Test requests

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_index_request_success_status_code(self):
+        """Should return status code 200 when request home index"""
+        self.assertAlmostEquals(200, self.response.status_code)
+
+    def test_index_default_template(self):
+        """Should render index.html as index template"""
+        self.assertTemplateUsed(self.response, 'index.html')
